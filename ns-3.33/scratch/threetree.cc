4fbbc40f1d09e3a67dec4113880b069007954484
bibek shrestha <sbibek@hotmail.com>
2021-09-16 13:16:26 -0700

    better formatting for the threetree

--- ns-3.33/scratch/threetree.cc
+++ ns-3.33/scratch/threetree.cc
@@ -18,12 +18,32 @@ using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("THREETREE");
 
-#define __DUMP_IPS 0 
-#define __ASCII_TRACE 1 
-#define __PCAP_TRACE 0 
+#define __DUMP_IPS 0
+#define __ASCII_TRACE 1
+#define __PCAP_TRACE 0
+
+/********* topology configurations ***************/
+int AGGREGATION_SW_N = 10;
+int EDGE_PER_AGG_SW_N = 2;
+int NODE_PER_EDGE = 2;
+// ------------------------
+
+// probing configurations
+uint16_t PROBE_PORT = 9999;
+uint16_t PROBE_SENDER_MAX_PACKETS = 0; // 0 = unlimited
+float PROBE_SENDER_INTERVAL_SEC = 0.1; // 100ms
+// --------------------------------------
+
+// dataplane switch configuration
+uint64_t ROLL_STATS_PERIOD_MS = PROBE_SENDER_INTERVAL_SEC * 1000;
+// ------------------------------------------------------------------
+
+// CSMA links configurations
+std::string LINK_DATA_RATE = "10Mbps";
+std::string QUEUE_SIZE = "100p";
+int DELAY_MICROSEC = 5;
 
 CsmaHelper createCsmaHelper (std::string dataRate, std::string queueSize, int delayMicroSec);
-
 
 /**
  *  Important** 
@@ -36,32 +56,14 @@ CsmaHelper createCsmaHelper (std::string dataRate, std::string queueSize, int de
 int
 main (int argc, char *argv[])
 {
-//   LogComponentEnable ("GtcpServer", LOG_LEVEL_DEBUG);
-//   LogComponentEnable ("BulkSendApplication2", LOG_LEVEL_DEBUG);
-//   LogComponentEnable ("GtcpClient", LOG_LEVEL_INFO);
-//   LogComponentEnable ("PScheduler", LOG_LEVEL_DEBUG);
-// LogComponentEnable ("BulkSendApplication", LOG_LEVEL_INFO);
+  //   LogComponentEnable ("GtcpServer", LOG_LEVEL_DEBUG);
+  //   LogComponentEnable ("BulkSendApplication2", LOG_LEVEL_DEBUG);
+  //   LogComponentEnable ("GtcpClient", LOG_LEVEL_INFO);
+  //   LogComponentEnable ("PScheduler", LOG_LEVEL_DEBUG);
+  // LogComponentEnable ("BulkSendApplication", LOG_LEVEL_INFO);
 
-  // topology configurations
-  int AGGREGATION_SW_N = 10;
-  int EDGE_PER_AGG_SW_N = 2;
-  int NODE_PER_EDGE = 2;
-  // ------------------------
+  /*############################ TOPOLOGY SECTION ############################*/
 
-  // probing configurations
-  uint16_t PROBE_PORT = 9999;
-  uint16_t PROBE_SENDER_MAX_PACKETS = 0; // 0 = unlimited
-  float PROBE_SENDER_INTERVAL_SEC = 0.1; // 100ms
-  // --------------------------------------
-
-  // dataplane switch configuration
-  uint64_t ROLL_STATS_PERIOD_MS = PROBE_SENDER_INTERVAL_SEC * 1000;
-  // ------------------------------------------------------------------
-
-  // CSMA links configurations
-  std::string LINK_DATA_RATE = "10Mbps";
-  std::string QUEUE_SIZE = "100p";
-  int DELAY_MICROSEC = 5;
   CsmaHelper generalCsmaHelper = createCsmaHelper (LINK_DATA_RATE, QUEUE_SIZE, DELAY_MICROSEC);
   // -----------------------------------------------------------------------------------------
 
@@ -144,15 +146,16 @@ main (int argc, char *argv[])
       edgeNetDevices[i].Add (link.Get (0));
       probingDevices.Add (link.Get (1));
     }
-    // end of adding probing nodes
+  // end of adding probing nodes
 
   // now we will create terminal edge nodes
   NodeContainer terminals;
   terminals.Create (AGGREGATION_SW_N * EDGE_PER_AGG_SW_N * NODE_PER_EDGE);
   // set terminal ids
-  for(int i=0;i<(int)terminals.GetN();i++) {
-    terminals.Get(i)->SetIdx(i);
-  }
+  for (int i = 0; i < (int) terminals.GetN (); i++)
+    {
+      terminals.Get (i)->SetIdx (i);
+    }
 
   // link all the terminals to the edges
   NetDeviceContainer terminalDevices;
@@ -183,23 +186,23 @@ main (int argc, char *argv[])
 
   // Install internet stack to all the terminal nodes
   InternetStackHelper internet;
-  internet.Install(probingNodes);
+  internet.Install (probingNodes);
   internet.Install (terminals);
 
   // now we give ip address to all the terminals
   Ipv4AddressHelper ipv4;
   ipv4.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer probeips = ipv4.Assign(probingDevices);
+  Ipv4InterfaceContainer probeips = ipv4.Assign (probingDevices);
   Ipv4InterfaceContainer terminalips = ipv4.Assign (terminalDevices);
-
 
   // updating the terminal nodes with ip information as required for our simulation
   // This is important because the nodes information is required for the data transfer operations
-  for(int i=0;i<(int)terminals.GetN();i++) {
-    terminals.Get(i)->SetTerminalIps(&terminalips);
-  }
+  for (int i = 0; i < (int) terminals.GetN (); i++)
+    {
+      terminals.Get (i)->SetTerminalIps (&terminalips);
+    }
 
-// now we will share terminalDeviecs to all the terminal nodes
+    // now we will share terminalDeviecs to all the terminal nodes
 
 #if __DUMP_IPS == 1
   std::cout << "[ Probing Nodes IPs ] \n";
@@ -218,115 +221,112 @@ main (int argc, char *argv[])
       std::cout << " " << i << " = " << peerAddressStringStream.str () << std::endl;
     }
 #endif
+    /*############################ END OF TOPOLOGY SECTION ############################*/
 
-  // uint16_t port = 9; // Discard port (RFC 863)
-  // Address sinkAddress (InetSocketAddress (ips.GetAddress (ips.GetN () - 1), port));
-  // PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory",
-  //                                    InetSocketAddress (Ipv4Address::GetAny (), port));
-  // ApplicationContainer sinkapp = packetSinkHelper.Install (terminals.Get (terminals.GetN () - 1));
-  // sinkapp.Start (Seconds (0));
-  // sinkapp.Stop (Seconds (10));
-
-  // BulkSendHelper source0 ("ns3::TcpSocketFactory", sinkAddress);
-  // ApplicationContainer sourceapp = source0.Install (probingNodes.Get (0));
-  // sourceapp.Start (Seconds (1));
-  // sourceapp.Stop (Seconds (10));
-
-
- #define SCHEDULER
- #ifdef SCHEDULER 
-  int _schid =  1;
+    /*############################ INSTALL SCHEDULER AND PROBE MODULE SECTION ############################*/
+#define SCHEDULER
+#ifdef SCHEDULER
+  int _schid = 1;
   // terminalips.GetN()-1;
   // we will last one as the scheduler node
-  PSchedulerHelper schedulerhelper(PROBE_PORT);
+  PSchedulerHelper schedulerhelper (PROBE_PORT);
   ns3::Address schedulerIpAddress = ns3::Address (terminalips.GetAddress (_schid));
 
-  ApplicationContainer schedulerApp = schedulerhelper.Install(terminals.Get(_schid));
-  schedulerApp.Start(Seconds(0.0));
-  schedulerApp.Stop(Seconds(50.0));
+  ApplicationContainer schedulerApp = schedulerhelper.Install (terminals.Get (_schid));
+  schedulerApp.Start (Seconds (0.0));
+  schedulerApp.Stop (Seconds (50.0));
 
-  
-  ProbeAppHelper probeapphelper(schedulerIpAddress, PROBE_PORT, PROBE_SENDER_MAX_PACKETS,
-                            PROBE_SENDER_INTERVAL_SEC);
+  ProbeAppHelper probeapphelper (schedulerIpAddress, PROBE_PORT, PROBE_SENDER_MAX_PACKETS,
+                                 PROBE_SENDER_INTERVAL_SEC);
   // NodeContainer probeNodes;
   // probeNodes.Add(probingNodes.Get(0));
   // probeNodes.Add(probingNodes.Get(10));
   // probeNodes.Add(probingNodes.Get(30));
-  ApplicationContainer app = probeapphelper.Install(
-    // probingNodes.Get(0)
-    probingNodes
-    // probeNodes
-    );
-  app.Start(Seconds(1));
-  app.Stop(Seconds(50));
+  ApplicationContainer app = probeapphelper.Install (
+      // probingNodes.Get(0)
+      probingNodes
+      // probeNodes
+  );
+  app.Start (Seconds (1));
+  app.Stop (Seconds (50));
 #endif
+  /*############################ END OF INSTALL SCHEDULER AND PROBE MODULE SECTION ############################*/
+
+  /*############################ OPTIONAL PATHTRACE MODULE ############################*/
 
 #ifdef PATH_TRACE
-  ns3::Address schedulerIpAddress = ns3::Address (terminalips.GetAddress (terminalips.GetN()-1));
-  ProbeAppHelper probeapphelper(schedulerIpAddress, PROBE_PORT+1, PROBE_SENDER_MAX_PACKETS+1,
-                            PROBE_SENDER_INTERVAL_SEC);
-  ApplicationContainer app = probeapphelper.Install(terminals.Get(0));
-  app.Start(Seconds(1));
-  app.Stop(Seconds(10));
+  ns3::Address schedulerIpAddress = ns3::Address (terminalips.GetAddress (terminalips.GetN () - 1));
+  ProbeAppHelper probeapphelper (schedulerIpAddress, PROBE_PORT + 1, PROBE_SENDER_MAX_PACKETS + 1,
+                                 PROBE_SENDER_INTERVAL_SEC);
+  ApplicationContainer app = probeapphelper.Install (terminals.Get (0));
+  app.Start (Seconds (1));
+  app.Stop (Seconds (10));
 #endif
+  /*############################ END OF OPTIONAL PATHTRACE MODULE ############################*/
 
-#define TEST_DATA_TRANSFER
-#ifdef TEST_DATA_TRANSFER
+  /*############################ OFFLOAD MODULE ############################*/
+#define OFFLOAD_MOD
+#ifdef OFFLOAD_MOD
   // data transfer application
   int dataTransferPort = 12345;
-  int _idx = terminalips.GetN()-1;
+  int _idx = terminalips.GetN () - 1;
   Address sinkAddress (InetSocketAddress (terminalips.GetAddress (_idx), dataTransferPort));
 
-  GtcpServerHelper gtcpserverHelper(dataTransferPort);
-  gtcpserverHelper.SetAttribute("WaitPeriod", TimeValue(Seconds(5)));
-  gtcpserverHelper.SetAttribute("TotalResponseBytes", UintegerValue(1024*1024));
+  GtcpServerHelper gtcpserverHelper (dataTransferPort);
+  gtcpserverHelper.SetAttribute ("WaitPeriod", TimeValue (Seconds (5)));
+  gtcpserverHelper.SetAttribute ("TotalResponseBytes", UintegerValue (1024 * 1024));
 
   PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory",
                                      InetSocketAddress (Ipv4Address::GetAny (), dataTransferPort));
 
-  ApplicationContainer sinkapp =  gtcpserverHelper.Install(terminals);
-                              //packetSinkHelper.Install (terminals.Get (_idx));
+  ApplicationContainer sinkapp = gtcpserverHelper.Install (terminals);
+  //packetSinkHelper.Install (terminals.Get (_idx));
   sinkapp.Start (Seconds (1));
-  sinkapp.Stop (Seconds(30));
-  
+  sinkapp.Stop (Seconds (30));
+
   BulkSendHelper2 source0;
   // GtcpClientHelper source0(Address(terminalips.GetAddress(_idx)), dataTransferPort);
   // source0.SetRemote(sinkAddress)
-  source0.SetAttribute ("MaxBytes", UintegerValue (1024*1024*2));
-  source0.SetAttribute("schedularAddress", AddressValue(schedulerIpAddress));
+  source0.SetAttribute ("MaxBytes", UintegerValue (1024 * 1024 * 2));
+  source0.SetAttribute ("schedularAddress", AddressValue (schedulerIpAddress));
   ApplicationContainer sourceapp = source0.Install (terminals.Get (0));
   sourceapp.Start (Seconds (10));
-  sourceapp.Stop (Seconds(30));
+  sourceapp.Stop (Seconds (30));
 #endif
+/*############################ END OF OFFLOAD MODULE ############################*/
 
+/*############################ BACKGROUND TRAFFIC ############################*/
 // we will schedule background data transfers from hosts to hosts
-#define BACKGROUND_DATA_TRANSFERS
+// #define BACKGROUND_DATA_TRANSFERS
 #ifdef BACKGROUND_DATA_TRANSFERS
   // we will install sink on all nodes
   int psinkPort = 9997;
   PacketSinkHelper psink ("ns3::TcpSocketFactory",
-                                     InetSocketAddress (Ipv4Address::GetAny (), psinkPort));
-  ApplicationContainer sinkapps = psink.Install(terminals);
-  sinkapps.Start(Seconds(0));
-  sinkapps.Stop(Seconds(50));
+                          InetSocketAddress (Ipv4Address::GetAny (), psinkPort));
+  ApplicationContainer sinkapps = psink.Install (terminals);
+  sinkapps.Start (Seconds (0));
+  sinkapps.Stop (Seconds (50));
 
-
-  std::vector<std::tuple<int,int, int, int>> backgroundTransfersMapTerminal2Terminal;
+  std::vector<std::tuple<int, int, int, int>> backgroundTransfersMapTerminal2Terminal;
   //for example, the transfer between 10 and 20 host start at 0s and stop at 30s
-  backgroundTransfersMapTerminal2Terminal.push_back(std::make_tuple(10, 20, 5, 30));
+  backgroundTransfersMapTerminal2Terminal.push_back (std::make_tuple (10, 20, 5, 30));
 
-  for(std::tuple<int, int, int, int> h2h: backgroundTransfersMapTerminal2Terminal) {
-    // this means the transfer is set between them (sender host, destination host, start time, end time)
-    BulkSendHelper psrc ("ns3::TcpSocketFactory",
-                          InetSocketAddress (terminalips.GetAddress(std::get<1>(h2h)), psinkPort));
-    psrc.SetAttribute ("MaxBytes", UintegerValue (1024*1024*2));
-    ApplicationContainer psrcapp = psrc.Install (terminals.Get (std::get<0>(h2h)));                      
-    psrcapp.Start(Seconds(std::get<2>(h2h)));
-    psrcapp.Stop(Seconds(std::get<3>(h2h)));
-  }
+  for (std::tuple<int, int, int, int> h2h : backgroundTransfersMapTerminal2Terminal)
+    {
+      // this means the transfer is set between them (sender host, destination host, start time, end time)
+      BulkSendHelper psrc (
+          "ns3::TcpSocketFactory",
+          InetSocketAddress (terminalips.GetAddress (std::get<1> (h2h)), psinkPort));
+      psrc.SetAttribute ("MaxBytes", UintegerValue (1024 * 1024 * 2));
+      ApplicationContainer psrcapp = psrc.Install (terminals.Get (std::get<0> (h2h)));
+      psrcapp.Start (Seconds (std::get<2> (h2h)));
+      psrcapp.Stop (Seconds (std::get<3> (h2h)));
+    }
 
 #endif
+/*############################ END OF BACKGROUND TRAFFIC ############################*/
 
+/*############################ TRACING ############################*/
 #if __ASCII_TRACE == 1
   AsciiTraceHelper ascii;
   generalCsmaHelper.EnableAsciiAll (ascii.CreateFileStream ("tree.tr"));
@@ -335,12 +335,14 @@ main (int argc, char *argv[])
 #if __PCAP_TRACE == 1
   generalCsmaHelper.EnablePcapAll ("tree", false);
 #endif
+  /*############################ END OF TRACING ############################*/
+
   Simulator::Stop (Seconds (50));
   Simulator::Run ();
   Simulator::Destroy ();
   NS_LOG_INFO ("Done.");
- Ptr<PacketSink> sink1 = DynamicCast<PacketSink> (sinkapps.Get (20));
-  std::cout << "Total Bytes Received: " << sink1->GetTotalRx () << std::endl;
+  //  Ptr<PacketSink> sink1 = DynamicCast<PacketSink> (sinkapps.Get (20));
+  // std::cout << "Total Bytes Received: " << sink1->GetTotalRx () << std::endl;
 }
 
 CsmaHelper
